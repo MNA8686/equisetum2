5e5c356d14e3b178cc7849a94d6dd2827dc07e8c
MNA <MNA@d00f1e05-d623-49aa-90c9-d044925e164e>
2017-03-24 15:30:13 +0000

    コメント追加
    git-svn-id: svn://192.168.0.2/equinox/project/trunk/Equisetum2@270 d00f1e05-d623-49aa-90c9-d044925e164e

--- include/stream/IStream.h
+++ include/stream/IStream.h
@@ -1,3 +1,9 @@
+﻿/**
+* @file IStream.h
+* @brief ストリームの抽象クラス
+* @author MNA
+* @date 2017/02/13 新規作成
+*/
 #if !defined(_EQSTREAM_H_)
 #define _EQSTREAM_H_
 
@@ -9,29 +15,123 @@
 
 namespace Equisetum2
 {
+	/**
+	* @enum SeekOrigin
+	* シーク方法の指定
+	*/
 	enum class SeekOrigin : int
 	{
-		Begin = 1,
-		Current,
-		End
+		Begin = 1,	/// ファイルの先頭から
+		Current,	/// 現在の位置から
+		End			/// ファイルの終端から
 	};
 
+	/**
+	* ストリームの抽象クラス。<br>
+	* ストリームを扱うクラスはこれを派生して使う。
+	*/
 	class IStream
 	{
 	public:
+		/**
+		* @brief read可能かどうか
+		* @return read可否
+		*/
 		virtual bool CanRead() const = 0;
+
+		/**
+		* @brief seek可能かどうか
+		* @return seek可否
+		*/
 		virtual bool CanSeek() const = 0;
+
+		/**
+		* @brief write可能かどうか
+		* @return write可否
+		*/
 		virtual bool CanWrite() const = 0;
+
+		/**
+		* @brief ストリーム内での現在位置を返す
+		* @return ストリーム内での現在位置
+		*/
 		virtual int64_t Position() const = 0;
+
+		/**
+		* @brief ストリームのサイズを返す
+		* @return ストリームのサイズ
+		*/
 		virtual int64_t Length() const = 0;
+
+		/**
+		* @brief 現在位置から終端までの内容を別のストリームに出力する
+		* @param stream 出力先ストリーム
+		* @return 成否
+		*
+		* @detail 書き込み先ストリームが書き込み可能でなければならない
+		*/
 		virtual bool CopyTo(std::shared_ptr<IStream> stream) = 0;
+
+		/**
+		* @brief Seekを行う
+		* @param offset オフセット
+		* @param origin 基準とする位置
+		* @return seek後の位置
+		*/
 		virtual int64_t Seek(int64_t offset, SeekOrigin origin) = 0;
+
+		/**
+		* @brief ストリームから読み出す
+		* @param vByteArray 読み出しデータ格納バッファ
+		* @param begin 読み出し開始位置
+		* @param size 読み出しバイト数
+		* @return null:読み出し失敗、>=0:読み出したバイト数
+		*/
 		virtual const Optional<size_t> Read(std::vector<uint8_t>& vByteArray, size_t begin, size_t size) = 0;
+
+		/**
+		* @brief ストリームから読み出す
+		* @param vByteArray 読み出しデータ格納バッファ
+		* @param size 読み出しバイト数
+		* @return null:読み出し失敗、>=0:読み出したバイト数
+		*/
 		virtual const Optional<size_t> Read(std::vector<uint8_t>& vByteArray, size_t size) = 0;
+
+		/**
+		* @brief ストリームに書き込む
+		* @param vByteArray 書き込みデータ格納バッファ
+		* @param begin 書き込み開始位置
+		* @param size 書き込みバイト数
+		* @return null:書き込み失敗、>=0:書き込んだバイト数
+		*/
 		virtual const Optional<size_t> Write(const std::vector<uint8_t>& vByteArray, size_t begin, size_t size) = 0;
+
+		/**
+		* @brief ストリームに書き込む
+		* @param vByteArray 書き込みデータ格納バッファ
+		* @param size 書き込みバイト数
+		* @return null:書き込み失敗、>=0:書き込んだバイト数
+		*/
 		virtual const Optional<size_t> Write(const std::vector<uint8_t>& vByteArray, size_t size) = 0;
+
+		/**
+		* @brief ストリームに書き込む
+		* @param vByteArray 書き込みデータ格納バッファ
+		* @return null:書き込み失敗、>=0:書き込んだバイト数
+		*/
 		virtual const Optional<size_t> Write(const std::vector<uint8_t>& vByteArray) = 0;
+
+		/**
+		* @brief 1バイト読み出す
+		* @return >=0:読み出した内容(下位1バイト)、<0:読み出し失敗
+		*/
 		virtual int ReadByte() = 0;
+
+		/**
+		* @brief 1バイト書き込む
+		* @param writeData 書き込みたいデータ
+		* @return 1:成功、1以外:失敗
+		*/
 		virtual int WriteByte(uint8_t writeData) = 0;
 	};
 }
