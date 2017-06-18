e70d59439e1c6a1e47e55517329c324b0e55770a
MNA <MNA@d00f1e05-d623-49aa-90c9-d044925e164e>
2017-06-18 12:21:15 +0000

    Regexの例外に対応
    git-svn-id: svn://192.168.0.2/equinox/project/trunk/Equisetum2@279 d00f1e05-d623-49aa-90c9-d044925e164e

--- src/common/util/Regex.cpp
+++ src/common/util/Regex.cpp
@@ -7,60 +7,101 @@
 
 #include "util/Regex.hpp"
 #include <regex>
+#include "system/Logger.h"
 
 namespace Equisetum2
 {
 	String Regex::ReplaceFirst(const String& input, const String& regex, const String& replacement)
 	{
-		std::regex re(regex);
-		return std::regex_replace(input.c_str(), re, replacement.c_str(), std::regex_constants::format_first_only);
+		String str;
+
+		try
+		{
+			std::regex re(regex);
+			str = std::regex_replace(input.c_str(), re, replacement.c_str(), std::regex_constants::format_first_only);
+		}
+		catch (const std::regex_error& err)
+		{
+			Logger::OutputError(String::Sprintf(u8"正規表現の内容に問題があります。[%s]", err.what()).c_str());
+		}
+
+		return str;
 	}
 
 	String Regex::ReplaceAll(const String& input, const String& regex, const String& replacement)
 	{
-		std::regex re(regex);
-		return std::regex_replace(input.c_str(), re, replacement.c_str());
+		String str;
+
+		try
+		{
+			std::regex re(regex);
+			str = std::regex_replace(input.c_str(), re, replacement.c_str());
+		}
+		catch (const std::regex_error& err)
+		{
+			Logger::OutputError(String::Sprintf(u8"正規表現の内容に問題があります。[%s]", err.what()).c_str());
+		}
+
+		return str;
 	}
 
 	Optional<std::vector<String>> Regex::Search(const String& input, const String& regex)
 	{
 		Optional<std::vector<String>> ret;
-		std::regex re(regex);
-		std::cmatch match;
 
-		if (std::regex_search(input.c_str(), match, re))
+		try
 		{
-			std::vector<String> v;
+			std::regex re(regex);
+			std::cmatch match;
 
-			// キャプチャ結果などをvectorに格納する
-			for (const auto& submatch : match)
+			if (std::regex_search(input.c_str(), match, re))
 			{
-				v.push_back(String(submatch));
-			}
+				std::vector<String> v;
 
-			ret = v;
+				// キャプチャ結果などをvectorに格納する
+				for (const auto& submatch : match)
+				{
+					v.push_back(String(submatch));
+				}
+
+				ret = v;
+			}
 		}
+		catch (const std::regex_error& err)
+		{
+			Logger::OutputError(String::Sprintf(u8"正規表現の内容に問題があります。[%s]", err.what()).c_str());
+		}
+
 		return ret;
 	}
 
 	Optional<std::vector<String>> Regex::Match(const String& input, const String& regex)
 	{
 		Optional<std::vector<String>> ret;
-		std::regex re(regex);
-		std::cmatch match;
 
-		if (std::regex_match(input.c_str(), match, re))
+		try
 		{
-			std::vector<String> v;
+			std::regex re(regex);
+			std::cmatch match;
 
-			// キャプチャ結果などをvectorに格納する
-			for (const auto& submatch : match)
+			if (std::regex_match(input.c_str(), match, re))
 			{
-				v.push_back(String(submatch));
-			}
+				std::vector<String> v;
 
-			ret = v;
+				// キャプチャ結果などをvectorに格納する
+				for (const auto& submatch : match)
+				{
+					v.push_back(String(submatch));
+				}
+
+				ret = v;
+			}
 		}
+		catch (const std::regex_error& err)
+		{
+			Logger::OutputError(String::Sprintf(u8"正規表現の内容に問題があります。[%s]", err.what()).c_str());
+		}
+
 		return ret;
 	}
 }
