6bedf2b0911c471847fa5ddbe1507734c8aa5b91
MNA <MNA@d00f1e05-d623-49aa-90c9-d044925e164e>
2017-09-26 08:57:16 +0000

    コミット漏れ
    git-svn-id: svn://192.168.0.2/equinox/project/trunk/Equisetum2@315 d00f1e05-d623-49aa-90c9-d044925e164e

---
+++ include/graphic/Image.hpp
@@ -0,0 +1,86 @@
+﻿#if !defined(_EQIMAGE_H_)
+#define _EQIMAGE_H_
+
+#include "stream/IStream.h"
+#include <memory>
+
+namespace Equisetum2
+{
+	union Color
+	{
+		struct rgba
+		{
+			uint8_t a;
+			uint8_t r;
+			uint8_t g;
+			uint8_t b;
+		};
+
+		uint32_t pixel;
+	};
+
+	class Image
+	{
+	public:
+
+		/**
+		* @brief ストリームからイメージを作成する BMP、PNGのみを正式サポート
+		* @param stream 入力元ストリーム
+		* @return 成功時イメージのポインタ
+		*
+		* @detail 入力元ストリームが読み出し可能でなければならない
+		*/
+		static std::shared_ptr<Image> CreateFromStream(std::shared_ptr<IStream> stream);
+
+		/**
+		* @brief ストリームに対してイメージ(PNG)を出力する
+		* @param stream 出力元ストリーム
+		* @return 成否
+		*
+		* @detail 出力先ストリームが書き込み及びシーク可能でなければならない
+		*/
+		bool SaveToStream(std::shared_ptr<IStream> stream);
+
+		/**
+		* @brief イメージのサイズを変更する
+		* @param width 変更後の横サイズ
+		* @param height 変更後の縦サイズ
+		* @return 成否
+		*/
+		bool Resize(uint32_t width, uint32_t height);
+
+		/**
+		* @brief イメージの横幅を取得する
+		* @return 成否
+		*/
+		uint32_t Width();
+
+		/**
+		* @brief イメージの縦幅を取得する
+		* @return 成否
+		*/
+		uint32_t Height();
+
+		/**
+		* @brief イメージの横幅をバイト数で取得する
+		* @return 成否
+		*/
+		uint32_t Pitch();
+
+		/**
+		* @brief イメージのデータの先頭ポインタを取得する
+		* @return イメージへのポインタ
+		*/
+		Color* Data();
+
+	protected:
+
+		Image() {}
+
+	private:
+
+		bool InitFromStream(std::shared_ptr<IStream>& stream);
+	};
+}
+
+#endif
