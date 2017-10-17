7665fe6df4aee3e50ceec005cf5958151b3255fe
MNA <MNA@d00f1e05-d623-49aa-90c9-d044925e164e>
2017-10-17 16:56:29 +0000

    VariableMemoryStreamのテスト追加
    git-svn-id: svn://192.168.0.2/equinox/project/trunk/Equisetum2@340 d00f1e05-d623-49aa-90c9-d044925e164e

--- unittest/test/Stream.cpp
+++ unittest/test/Stream.cpp
@@ -188,3 +188,145 @@ TEST(PartialStream, CreateFromStream)
 		ASSERT_FALSE(partial);
 	}
 }
+
+TEST(VariableMemoryStream, Basic)
+{
+	std::vector<uint8_t> buf(256);
+
+	uint8_t i = 2;
+	for (auto& elem : buf)
+	{
+		elem = i;
+		i++;
+	}
+
+	auto vstream = VariableMemoryStream::Create();
+	ASSERT_TRUE(vstream);
+
+	ASSERT_EQ(0, vstream->Position());
+	ASSERT_EQ(0, vstream->Length());
+
+	vstream->Write(buf.data(), 5);
+	vstream->Seek(0, SeekOrigin::Begin);
+
+	ASSERT_EQ(0, vstream->Position());
+	ASSERT_EQ(5, vstream->Length());
+
+	ASSERT_EQ(2, vstream->ReadByte());
+	ASSERT_EQ(3, vstream->ReadByte());
+	ASSERT_EQ(4, vstream->ReadByte());
+	ASSERT_EQ(5, vstream->ReadByte());
+	ASSERT_EQ(6, vstream->ReadByte());
+	ASSERT_TRUE(vstream->ReadByte() < 0);
+	ASSERT_TRUE(vstream->ReadByte() < 0);
+	ASSERT_TRUE(vstream->ReadByte() < 0);
+	ASSERT_EQ(5, vstream->Position());
+
+	ASSERT_EQ(5, vstream->Seek(1, SeekOrigin::Current));
+	ASSERT_EQ(5, vstream->Seek(0, SeekOrigin::Current));
+	ASSERT_EQ(4, vstream->Seek(-1, SeekOrigin::Current));
+	ASSERT_EQ(3, vstream->Seek(-1, SeekOrigin::Current));
+	ASSERT_EQ(2, vstream->Seek(-1, SeekOrigin::Current));
+	ASSERT_EQ(1, vstream->Seek(-1, SeekOrigin::Current));
+	ASSERT_EQ(0, vstream->Seek(-1, SeekOrigin::Current));
+	ASSERT_EQ(0, vstream->Seek(-1, SeekOrigin::Current));
+
+	ASSERT_EQ(1, vstream->Seek(1, SeekOrigin::Current));
+	ASSERT_EQ(2, vstream->Seek(1, SeekOrigin::Current));
+	ASSERT_EQ(3, vstream->Seek(1, SeekOrigin::Current));
+	ASSERT_EQ(4, vstream->Seek(1, SeekOrigin::Current));
+	ASSERT_EQ(5, vstream->Seek(1, SeekOrigin::Current));
+	ASSERT_EQ(5, vstream->Seek(1, SeekOrigin::Current));
+	ASSERT_EQ(0, vstream->Seek(-100, SeekOrigin::Current));
+	ASSERT_EQ(3, vstream->Seek(3, SeekOrigin::Current));
+	ASSERT_EQ(5, vstream->Seek(100, SeekOrigin::Current));
+
+	ASSERT_EQ(0, vstream->Seek(-1, SeekOrigin::Begin));
+	ASSERT_EQ(0, vstream->Seek(0, SeekOrigin::Begin));
+	ASSERT_EQ(1, vstream->Seek(1, SeekOrigin::Begin));
+	ASSERT_EQ(5, vstream->Seek(5, SeekOrigin::Begin));
+	ASSERT_EQ(5, vstream->Seek(6, SeekOrigin::Begin));
+
+	ASSERT_EQ(5, vstream->Seek(1, SeekOrigin::End));
+	ASSERT_EQ(5, vstream->Seek(0, SeekOrigin::End));
+	ASSERT_EQ(4, vstream->Seek(-1, SeekOrigin::End));
+	ASSERT_EQ(0, vstream->Seek(-5, SeekOrigin::End));
+	ASSERT_EQ(0, vstream->Seek(-6, SeekOrigin::End));
+
+	std::vector<uint8_t> v(1024);
+	memset(&v[0], 0x5A, v.size());
+
+	auto readResult = vstream->Read(&v[0], 100);
+	ASSERT_TRUE(readResult);
+	ASSERT_EQ(5, *readResult);
+
+	ASSERT_EQ(2, v[0]);
+	ASSERT_EQ(3, v[1]);
+	ASSERT_EQ(4, v[2]);
+	ASSERT_EQ(5, v[3]);
+	ASSERT_EQ(6, v[4]);
+	ASSERT_EQ(0x5A, v[5]);
+
+	{
+		auto writeResult = vstream->Write(v.data(), 100);
+		ASSERT_TRUE(writeResult);
+		ASSERT_EQ(100, *writeResult);
+		ASSERT_EQ(105, vstream->Position());
+	}
+	{
+		auto writeResult = vstream->Write(v.data(), 1024);
+		ASSERT_TRUE(writeResult);
+		ASSERT_EQ(1024, *writeResult);
+		ASSERT_EQ(105 + 1024, vstream->Position());
+		ASSERT_EQ(105 + 1024, vstream->Length());
+	}
+	{
+		ASSERT_EQ(500, vstream->Seek(500, SeekOrigin::Begin));
+
+		auto writeResult = vstream->Write(v.data(), 1024);
+		ASSERT_TRUE(writeResult);
+		ASSERT_EQ(1024, *writeResult);
+		ASSERT_EQ(500 + 1024, vstream->Position());
+		ASSERT_EQ(500 + 1024, vstream->Length());
+	}
+	{
+		ASSERT_EQ(100, vstream->Seek(100, SeekOrigin::Begin));
+
+		auto writeResult = vstream->Write(v.data(), 1024);
+		ASSERT_TRUE(writeResult);
+		ASSERT_EQ(1024, *writeResult);
+		ASSERT_EQ(100 + 1024, vstream->Position());
+		ASSERT_EQ(500 + 1024, vstream->Length());
+	}
+	{
+		ASSERT_EQ(500, vstream->Seek(500, SeekOrigin::Begin));
+
+		auto writeResult = vstream->Write(v.data(), 1024);
+		ASSERT_TRUE(writeResult);
+		ASSERT_EQ(1024, *writeResult);
+		ASSERT_EQ(500 + 1024, vstream->Position());
+		ASSERT_EQ(500 + 1024, vstream->Length());
+	}
+	{
+		ASSERT_EQ(501, vstream->Seek(501, SeekOrigin::Begin));
+
+		auto writeResult = vstream->Write(v.data(), 1024);
+		ASSERT_TRUE(writeResult);
+		ASSERT_EQ(1024, *writeResult);
+		ASSERT_EQ(501 + 1024, vstream->Position());
+		ASSERT_EQ(501 + 1024, vstream->Length());
+	}
+	{
+		std::vector<uint8_t> v(10240);
+
+		ASSERT_EQ(0, vstream->Seek(0, SeekOrigin::Begin));
+		ASSERT_EQ(0, vstream->Position());
+		ASSERT_EQ(501 + 1024, vstream->Length());
+
+		auto readResult = vstream->Read(v.data(), 10240);
+		ASSERT_TRUE(readResult);
+		ASSERT_EQ(501 + 1024, *readResult);
+		ASSERT_EQ(501 + 1024, vstream->Position());
+		ASSERT_EQ(501 + 1024, vstream->Length());
+	}
+}
