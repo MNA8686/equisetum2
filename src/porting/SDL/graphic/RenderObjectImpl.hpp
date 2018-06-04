8a693ecef0da4510512c5e36dea2c8b76597907e
MNA <mna@mna.net>
2018-06-04 19:54:46 +0900

    頂点とインデックス追加

--- src/porting/SDL/graphic/RenderObjectImpl.hpp
+++ src/porting/SDL/graphic/RenderObjectImpl.hpp
@@ -3,6 +3,7 @@
 
 #include "SDL.h"
 #include "graphic/Renderer.hpp"
+#include "graphic/RendererImpl.hpp"
 #include "graphic/RenderObject.hpp"
 #include "graphic/Sprite.hpp"
 #include "glew.h"
@@ -11,8 +12,15 @@ namespace Equisetum2
 {
 	class SpriteRenderer::Impl final
 	{
+	public:
+		const stVertex& GetVertex() const;
+		const GLushort& GetIndex() const;
+
 	private:
 		friend class SpriteRenderer;
+
+		stVertex m_vertex[4] = {};		/// 頂点数
+		GLushort m_index[6] = {};		/// インデックス数
 	};
 }
 
