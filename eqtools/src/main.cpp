caeb50e2156e97ba2d28d721f9697b72bfe747e4
MNA <mna@mna.net>
2018-12-05 01:29:46 +0900

    eqtoolsプロジェクト作成

---
+++ eqtools/src/main.cpp
@@ -0,0 +1,21 @@
+﻿#include "Equisetum2.h"
+
+using namespace Equisetum2;
+
+int Main()
+{
+	Logger::SetPriority(LogLevel::Info);
+	Logger::SetCallback([](LogLevel level, const char* str)->bool {
+		printf("%s\n", str);
+		return false;
+	});
+
+	int i = 0;
+	for (auto& arg : Argument())
+	{
+		printf("[%d] %s\n", i, arg.c_str());
+		i++;
+	}
+
+	return 0;
+}
