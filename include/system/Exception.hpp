90e942d461cc3bceffc19333ab20d619b5f20775
MNA <mna@mna.net>
2018-12-23 19:32:14 +0900

    ログのファイル名出力時、パスは除去するように修正

--- include/system/Exception.hpp
+++ include/system/Exception.hpp
@@ -2,6 +2,7 @@
 #define _EQEXCEPTION_H_
 
 #include "type/String.h"
+#include "fs/Path.hpp"
 
 namespace Equisetum2
 {
@@ -14,7 +15,7 @@ namespace Equisetum2
 		Exception(const String& msg, const String& file, int line, const String& function) :
 			m_msg(msg), m_file(file), m_line(line), m_function(function)
 		{
-			m_what.format("%s (%d): In function '%s()': %s", m_file.c_str(), m_line, m_function.c_str(), m_msg.c_str());
+			m_what.format("%s (%d): In function '%s()': %s", Path::GetFileName(m_file).c_str(), m_line, m_function.c_str(), m_msg.c_str());
 		}
 
 		const char* what() const noexcept override
