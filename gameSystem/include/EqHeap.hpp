b64c0e7372448d8dd01e0ef010201c1e7fcce690
MNA <mna@mna.net>
2019-08-05 01:17:56 +0900

    シリアライズ可能なヒープ機能を追加

---
+++ gameSystem/include/EqHeap.hpp
@@ -0,0 +1,218 @@
+#if !defined(_EQHEAP_H_)
+#define _EQHEAP_H_
+
+#include "Equisetum2.h"
+using namespace Equisetum2;
+
+class EqHeap
+{
+public:
+	using Handler = uint32_t;
+	friend Singleton<EqHeap>;	// シングルトンからインスタンスを作成してもらえるようにする
+
+	template<typename T>
+	class Container
+	{
+	public:
+		explicit Container() = default;
+
+		explicit Container(Handler handler)
+		{
+			auto heap = Singleton<EqHeap>::GetInstance();
+
+			if (heap->AddRef(handler) > 0)
+			{
+				m_handler = handler;
+
+				if (std::is_class<T>::value)
+				{
+					CallConstructor(Ref(), heap->Size(handler) / sizeof(T));
+				}
+			}
+		}
+
+		Container(const Container<T>& src)
+		{
+			auto heap = Singleton<EqHeap>::GetInstance();
+
+			if (heap->Test(src.m_handler))
+			{
+				heap->AddRef(src.m_handler);
+				m_handler = src.m_handler;
+			}
+		}
+
+		Container<T>& operator =(const Container<T>& src)
+		{
+			auto heap = Singleton<EqHeap>::GetInstance();
+
+			if (heap->DecRef(m_handler) == 0)
+			{
+				if (std::is_class<T>::value)
+				{
+					CallDestructor(Ref(), heap->Size(m_handler) / sizeof(T));
+				}
+
+				heap->Delete(m_handler);
+			}
+
+			if (heap->AddRef(src.m_handler) > 0)
+			{
+				m_handler = src.m_handler;
+			}
+			else
+			{
+				m_handler = 0;
+			}
+
+			return (*this);
+		}
+
+		T* Ref()
+		{
+			auto heap = Singleton<EqHeap>::GetInstance();
+			return heap->Ref<T>(m_handler);
+		}
+
+		~Container()
+		{
+			auto heap = Singleton<EqHeap>::GetInstance();
+			if (heap->DecRef(m_handler) == 0)
+			{
+				if (std::is_class<T>::value)
+				{
+					CallDestructor(Ref(), heap->Size(m_handler) / sizeof(T));
+				}
+
+				heap->Delete(m_handler);
+			}
+		}
+
+		operator Handler () const
+		{
+			return m_handler;
+		}
+
+	private:
+		void CallConstructor(T* ref, uint32_t num)
+		{
+			for (uint32_t i = 0; i < num; i++)
+			{
+				new (ref) T();
+				ref++;
+			}
+		}
+
+		void CallDestructor(T* ref, uint32_t num)
+		{
+			for (uint32_t i = 0; i < num; i++)
+			{
+				ref->~T();
+				ref++;
+			}
+		}
+
+		Handler m_handler = 0;
+	};
+
+	// 弱い参照用コンテナ
+	template<typename T>
+	class WeakRef
+	{
+	public:
+		explicit WeakRef() = default;
+
+		WeakRef(const Container<T>& src)
+		{
+			auto heap = Singleton<EqHeap>::GetInstance();
+
+			if (heap->Test(src.m_handler))
+			{
+				m_handler = src.m_handler;
+			}
+		}
+
+		WeakRef<T>& operator =(const Container<T>& src)
+		{
+			auto heap = Singleton<EqHeap>::GetInstance();
+
+			if (heap->Test(src))
+			{
+				m_handler = src;
+			}
+
+			return (*this);
+		}
+
+		T* Ref()
+		{
+			auto heap = Singleton<EqHeap>::GetInstance();
+			return heap->Ref<T>(m_handler);
+		}
+
+		~WeakRef() = default;
+
+		operator Handler () const
+		{
+			return m_handler;
+		}
+
+	private:
+
+		Handler m_handler = 0;
+	};
+
+protected:
+
+	typedef struct
+	{
+		Handler handler;
+		uint32_t size;
+		int32_t refCount;
+		void* ptr;
+	}stHeap;
+
+	std::vector<stHeap> m_vHandler;
+	int32_t m_reservedSize = 0;
+
+	int32_t AddRef(Handler handler);
+	int32_t DecRef(Handler handler);
+
+public:
+	bool InitHeapSystem(int32_t maxHandlerSize, int32_t reservedSize = 0);
+
+	EqHeap() = default;
+	~EqHeap();
+
+	template<typename T>
+	Container<T> New(uint32_t num = 1)
+	{
+		auto handler = New(sizeof(T) * num);
+		return Container<T>(handler);
+	}
+	Handler New(uint32_t allocSize);
+	void Delete(Handler handler);
+	bool Swap(Handler handler1, Handler handler2);
+
+	template<typename T>
+	T* Ref(Handler handler) const
+	{
+		return static_cast<T*>(Ref(handler));
+	}
+
+	int32_t UsedHandlerNum() const;
+	int32_t Size(Handler handler) const;
+	void* Ref(Handler handler) const;
+	bool Test(Handler handler) const;
+
+	bool Load(std::shared_ptr<IStream> in);
+	bool Save(std::shared_ptr<IStream> out);
+
+	template<class T>
+	struct isContainer : std::false_type {};
+
+	template<class T>
+	struct isContainer<Container<T>> : std::true_type {};
+
+}; 
+#endif
---
+++ gameSystem/src/EqHeap.cpp
@@ -0,0 +1,217 @@
+#include "EqHeap.hpp"
+
+EqHeap::~EqHeap()
+{
+	// きちんと解放が行われていればここでやることは無いはずである
+	// メモリリーク検出を行う？
+	//InitHeapSystem(0);
+
+	if (UsedHandlerNum() > 0)
+	{
+		Logger::OutputDebug("memory leak!");
+	}
+}
+
+bool EqHeap::InitHeapSystem(int32_t maxHandlerSize, int32_t reservedSize)
+{
+	// 全解放
+	for (auto& slot : m_vHandler)
+	{
+		if (slot.ptr)
+		{
+			free(slot.ptr);
+		}
+	}
+
+	m_vHandler.resize(maxHandlerSize);
+
+	uint32_t index = 0;
+
+	for (auto& slot : m_vHandler)
+	{
+		// 上位16ビットはハンドラの番号、下位16ビットはシリアル番号
+		slot.handler = (index << 16) | 1;
+		slot.size = 0;
+		slot.refCount = 0;
+		slot.ptr = nullptr;
+
+		index++;
+	}
+
+	m_reservedSize = reservedSize;
+
+	return true;
+}
+
+EqHeap::Handler EqHeap::New(uint32_t allocSize)
+{
+	Handler handler = 0;
+
+	if (allocSize != 0)
+	{
+		for (auto& slot : m_vHandler)
+		{
+			if (!slot.ptr)
+			{
+				slot.ptr = malloc(allocSize);
+				slot.size = allocSize;
+				memset(slot.ptr, 0, allocSize);
+				handler = slot.handler;
+				break;
+			}
+		}
+	}
+
+	return handler;
+}
+
+void* EqHeap::Ref(Handler handler) const
+{
+	if (handler != 0)
+	{
+		uint32_t index = handler >> 16;
+		auto& slot = m_vHandler[index];
+
+		if (slot.handler == handler)
+		{
+			return slot.ptr;
+		}
+	}
+
+	return nullptr;
+}
+
+int32_t EqHeap::AddRef(Handler handler)
+{
+	if (handler != 0)
+	{
+		uint32_t index = handler >> 16;
+		auto& slot = m_vHandler[index];
+
+		if (slot.handler == handler)
+		{
+			slot.refCount++;
+			return slot.refCount;
+		}
+	}
+
+	return -1;
+}
+
+int32_t EqHeap::DecRef(Handler handler)
+{
+	if (handler != 0)
+	{
+		uint32_t index = handler >> 16;
+		auto& slot = m_vHandler[index];
+
+		if (slot.handler == handler)
+		{
+			slot.refCount--;
+			return slot.refCount;
+		}
+	}
+
+	return -1;
+}
+
+// ハンドラに割り付けられているメモリのサイズ
+int32_t EqHeap::Size(Handler handler) const
+{
+	if (handler != 0)
+	{
+		uint32_t index = handler >> 16;
+		auto& slot = m_vHandler[index];
+
+		if (slot.handler == handler)
+		{
+			return slot.size;
+		}
+	}
+
+	return 0;
+}
+
+// ハンドラが有効かどうか調べる
+bool EqHeap::Test(Handler handler) const
+{
+	if (handler != 0)
+	{
+		uint32_t index = handler >> 16;
+		auto& slot = m_vHandler[index];
+
+		if (slot.handler == handler)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+// 実体をすり替える
+bool EqHeap::Swap(Handler handler1, Handler handler2)
+{
+	if (handler1 == handler2)
+	{
+		return true;
+	}
+
+	if (!Test(handler1) || !Test(handler2))
+	{
+		return false;
+	}
+
+	uint32_t index1 = handler1 >> 16;
+	auto& slot1 = m_vHandler[index1];
+	uint32_t index2 = handler2 >> 16;
+	auto& slot2 = m_vHandler[index2];
+
+	std::swap(slot1.ptr, slot2.ptr);
+	std::swap(slot1.size, slot2.size);
+
+	return true;
+}
+
+// ハンドラを削除し、メモリを解放する
+void EqHeap::Delete(Handler handler)
+{
+	if (handler != 0)
+	{
+		uint32_t index = handler >> 16;
+		auto& slot = m_vHandler[index];
+
+		if (slot.handler == handler)
+		{
+			free(slot.ptr);
+			slot.ptr = nullptr;
+			slot.size = 0;
+			//memset(slot.id, 0, sizeof(slot.id));
+
+			// 次回のためにシリアル番号をインクリメント(0は避ける)
+			uint32_t nextSerial = ((handler + 1) & 0x0000ffff);
+			if (nextSerial == 0)
+			{
+				nextSerial = 1;
+			}
+
+			slot.handler = (handler & 0xffff0000) | nextSerial;
+		}
+	}
+}
+
+int32_t EqHeap::UsedHandlerNum() const
+{
+	int32_t num = 0;
+
+	for (auto& slot : m_vHandler)
+	{
+		if (slot.ptr != 0)
+		{
+			num++;
+		}
+	}
+
+	return num;
+}
+
